3273e90abdd3f4cd07f94850941f0e9bca9ff755
suesitran <17781268+suesitran@users.noreply.github.com>
2023-01-27 15:44:43 +1100

    mock up the UI with CustomScrollView and its children

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -6,6 +6,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: CustomScrollView(
+            slivers: [
+              const SliverToBoxAdapter(
+                child: DateAndSummaryView(),
+              ),
+              SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+                if (index == 0) {
+                  return Text('Incomplete title');
+                }
+                return Text('Incomplete child');
+              },
+              childCount: 8)),
+              SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+                if (index == 0) {
+                  return Text('Completed title');
+                }
+
+                return Text('completed child')
+              }))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+class DateAndSummaryView extends StatelessWidget {
+  const DateAndSummaryView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('March 9, 2020', style: Theme.of(context).textTheme.headline4,)
+      ],
+    );
+  }
+}
+
