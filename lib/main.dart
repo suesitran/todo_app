2e61debbc91ebce3df1def81b3d1fb8318e51a27
suesitran <17781268+suesitran@users.noreply.github.com>
2023-01-27 18:04:54 +1100

    provide global bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/bloc/main_bloc.dart';
 import 'package:todo_app/features/home/home_page.dart';
 import 'package:todo_app/theme/color.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App Demo',
-      // The UI has dark and light theme, but for this demo, we're going to use only light theme
-      theme: ThemeData(
-        primarySwatch: primarySwatch,
+    return BlocProvider<MainBloc>(
+      create: (context) => MainBloc(),
+      child: MaterialApp(
+        title: 'Todo App Demo',
+        // The UI has dark and light theme, but for this demo, we're going to use only light theme
+        theme: ThemeData(
+          primarySwatch: primarySwatch,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
