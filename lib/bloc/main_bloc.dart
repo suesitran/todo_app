da2271baf6c7803797e2e86d62c90c7495e53e55
suesitran <17781268+suesitran@users.noreply.github.com>
2023-01-29 20:40:41 +1100

    initialise Hive and register TypeAdapter

--- lib/bloc/main_bloc.dart
+++ lib/bloc/main_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:todo_app/data/task.dart';
 
 part 'main_event.dart';
 part 'main_state.dart';
 
 class MainBloc extends Bloc<MainEvent, MainState> {
   MainBloc() : super(MainInitial()) {
+    _initialise();
+
     on<MainEvent>((event, emit) {
-      // TODO: implement event handler
     });
+  }
+
+  Future<void> _initialise() async {
+    // init Hive
+    await Hive.initFlutter();
+
+    // register Hive TypeAdapters
+    Hive.registerAdapter(TaskAdapter());
   }
 }
